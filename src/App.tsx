5dd8ed8c66308e71c901f429230c887394c558ba
Tanya Klevanets <t.klevanets@gmail.com>
2021-06-17 22:24:55 +0300

    BL-7 Подключить роутинг и для теста сделать заготовку страницы пользователя

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { FC, memo } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserPage } from '@pages/UserPage';
 
-export const App: FC = memo(() => {
-    const label = 'App';
-
-    return <div>{label}</div>;
-});
+export const App: FC = memo(() => (
+  <Router>
+    <Switch>
+      <Route path="/user">
+        <UserPage />
+      </Route>
+    </Switch>
+  </Router>
+));
