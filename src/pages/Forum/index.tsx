e5842fd6dde25bbfcabc8431837d62907ddde9dd
Olga Kopyova <imkopyova@gmail.com>
2021-06-21 19:21:04 +0300

    add key for Forum elements

--- src/pages/Forum/index.tsx
+++ src/pages/Forum/index.tsx
@@ -10,7 +10,16 @@ export const Forum: FC = memo(() => {
     <>
       <PageHeader title="forum"><Button variant="text" color="primary" onClick={handleFormCardClick}>+ create new</Button></PageHeader>
       <Box display="flex" flexWrap="wrap" justifyContent="space-between">
-        {forumCardsData.map((card) => <Box mb="60px"><ForumCard title={card.title} date={card.date} author={card.author} answers={card.answers} /></Box>)}
+        {forumCardsData.map((card) => (
+          <Box mb="60px" key={card.id}>
+            <ForumCard
+              title={card.title}
+              date={card.date}
+              author={card.author}
+              answers={card.answers}
+            />
+          </Box>
+        ))}
       </Box>
     </>
   );
