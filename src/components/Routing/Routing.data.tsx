25cb460e061e1f061ac61637fe0f710b1ec65b1c
Tatiana Klevanets <t.klevanets@gmail.com>
2021-06-27 13:43:01 +0300

    Настроить роуты для страниц (#45)

--- src/components/Routing/Routing.data.tsx
+++ src/components/Routing/Routing.data.tsx
@@ -1,10 +1,18 @@
 import { Discussion } from '@pages/Discussion';
 import { Forum } from '@pages/Forum';
+import { Game } from '@pages/Game';
+import { Leaderboard } from '@pages/Leaderboard';
 import { Login } from '@pages/Login';
 import { SignUp } from '@pages/SignUp';
+import { UserPage } from '@pages/UserPage';
 import { RoutesType } from './Routing.types';
 
 export const ROUTES: RoutesType = {
+  game: {
+    path: '/',
+    component: Game,
+    name: 'game',
+  },
   forum: {
     path: '/forum',
     component: Forum,
@@ -27,12 +35,12 @@ export const ROUTES: RoutesType = {
   },
   profile: {
     path: '/profile',
-    component: Forum,
+    component: UserPage,
     name: 'profile',
   },
   leaderboard: {
     path: '/leaderboard',
-    component: Forum,
+    component: Leaderboard,
     name: 'leaderboard',
   },
 };
