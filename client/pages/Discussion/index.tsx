8fd7d9ec934542d2bc19d05acbd548523389f300
Olga Kopyova <imkopyova@gmail.com>
2021-08-16 20:39:50 +0300

    fix: исправления после ревью

--- client/pages/Discussion/index.tsx
+++ client/pages/Discussion/index.tsx
@@ -5,6 +5,9 @@ import { withAuth } from '@core/HOKs/withAuth';
 import { useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { FormInput } from 'client/components/FormInput';
+import { v4 as uuid } from 'uuid';
+import { useSelector } from 'react-redux';
+import { profileSelector } from '@core/store';
 import { discussionData } from './discussion.mock';
 import { topicData } from '../Forum/forum.mock';
 
@@ -14,12 +17,13 @@ export const WrapperDiscussion: FC = memo(() => {
 
   const { handleSubmit, control, reset } = useForm();
 
-  const onSubmit = ({ text }: { [key: string]: string }) => {
+  const onSubmit = ({ comment }: { [key: string]: string }) => {
+    const profile = useSelector(profileSelector);
     const commentDate = new Date().toLocaleDateString('en-gb');
     const newComment = {
-      id: Date.now().toString(),
-      comment: text,
-      author: 'username',
+      id: uuid(),
+      comment,
+      author: profile.display_name,
       date: commentDate,
     };
 
@@ -28,56 +32,54 @@ export const WrapperDiscussion: FC = memo(() => {
   };
 
   return (
-    <>
-      <Paper elevation={22}>
-        <Box px="53px" py="46px" width="580px">
-          <Typography variant="body1">{mockTopic.date}</Typography>
-          <Box my="33px" height="118px">
-            <Typography variant="h6">{mockTopic.title}</Typography>
-            <Typography variant="body1">{mockTopic.text}</Typography>
-          </Box>
-          <Typography variant="body1">created by {mockTopic.author}</Typography>
+    <Paper elevation={22}>
+      <Box px="53px" py="46px" width="580px">
+        <Typography variant="body1">{mockTopic.date}</Typography>
+        <Box my="33px" height="118px">
+          <Typography variant="h6">{mockTopic.title}</Typography>
+          <Typography variant="body1">{mockTopic.text}</Typography>
         </Box>
-        <Box px="100px" py="80px">
-          <Box>
+        <Typography variant="body1">created by {mockTopic.author}</Typography>
+      </Box>
+      <Box px="100px" py="80px">
+        <Box>
+          <Message
+            type={MessageEnum.Question}
+            text={discussionData.text}
+            author={discussionData.author}
+            date={discussionData.date}
+          />
+        </Box>
+        <Box>
+          {discussionData.answers.map((answer) => (
             <Message
-              type={MessageEnum.Question}
-              text={discussionData.text}
-              author={discussionData.author}
-              date={discussionData.date}
+              type={MessageEnum.Answer}
+              text={answer.text}
+              author={answer.author}
+              date={answer.date}
             />
-          </Box>
-          <Box>
-            {discussionData.answers.map((answer) => (
-              <Message
-                type={MessageEnum.Answer}
-                text={answer.text}
-                author={answer.author}
-                date={answer.date}
-              />
               ))}
-          </Box>
-          <form
-            name="new_comment_form"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <Box display="flex">
-              <FormInput
-                inputName="text"
-                control={control}
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-              >
-                comment
-              </Button>
-            </Box>
-          </form>
         </Box>
-      </Paper>
-    </>
+        <form
+          name="new_comment_form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Box display="flex">
+            <FormInput
+              inputName="text"
+              control={control}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              comment
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Paper>
   );
 });
 
