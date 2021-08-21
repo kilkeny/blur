9e651c769da5eebf97740ffb000a94f95f7c2134
Ayrat Mustaev <aimustaev@avito.ru>
2021-08-22 01:05:30 +0300

    update

--- client/components/GameCanvas/Canvas.tsx
+++ client/components/GameCanvas/Canvas.tsx
@@ -1,5 +1,5 @@
 import { makeStyles, Theme } from '@material-ui/core';
-import React, { memo, FC } from 'react';
+import React, { memo, FC, useState } from 'react';
 import { GamePainter } from './Canvas.draw';
 import { useCanvas } from './useCanvas';
 
@@ -13,24 +13,35 @@ export type CanvasProps = {
   draw: GamePainter;
   handleGameOver: Function;
 };
+function fib(n:number): any {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
 
 export const Canvas: FC<CanvasProps> = memo(
   ({ draw, handleGameOver }: CanvasProps) => {
     const classes = useStyles();
     const canvasRef = useCanvas(draw, handleGameOver);
-
+    const [state, setState] = useState(20);
     const handleSetFullScreen = () => {
       if (canvasRef?.current) {
         canvasRef.current.requestFullscreen();
       }
     };
 
+    setInterval(() => {
+      setState(state + 1);
+    }, 10);
+
     return (
-      <canvas
-        ref={canvasRef}
-        className={classes.root}
-        onDoubleClick={handleSetFullScreen}
-      />
+      <>
+        <span style={{ position: 'absolute' }}>{fib(state)}</span>
+        <canvas
+          ref={canvasRef}
+          className={classes.root}
+          onDoubleClick={handleSetFullScreen}
+        />
+      </>
     );
   },
 );
