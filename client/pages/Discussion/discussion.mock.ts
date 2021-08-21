4d8da4ad9ef9f207ffa1f1c0353bcb2d7ed2fc39
Olga Kopyova <imkopyova@gmail.com>
2021-08-22 02:31:39 +0300

    fix: review fixes

--- client/pages/Discussion/discussion.mock.ts
+++ client/pages/Discussion/discussion.mock.ts
@@ -4,11 +4,13 @@ export const discussionData = {
   author: 'user',
   answers: [
     {
+      id: '1',
       text: 'Sed ut perspiciatis unde',
       date: 'may 12, 2021',
       author: 'user_1',
     },
     {
+      id: '2',
       text: 'Sed ut perspiciatis unde natus error sit voluptatem accusantium doloremqu',
       date: 'may 15, 2021',
       author: 'user_2',
